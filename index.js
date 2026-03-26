5c8307ba9fafaf59fcf713e0ad84979798e1af52
Ana Chicaiza <anachicaiza@icloud.com>
2026-03-26 15:40:23 +0000

    2 main function created
    update city: this functions targets the selection option in the website and it takes the city chosen  from this selection and show it in the page.
    
    updateTime: get  the current time  and date (as "23rd March 2026" / "8:39:16 AM") from  3 different cities.

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
-function sayHello() {
-    alert("Helloooooooo")
+function updateTime() {
+    let losAngelesElement = document.querySelector("#los-angeles");
+    if (losAngelesElement) {
+        let losAngelesDateElement = losAngelesElement.querySelector(".date");
+        let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+        let losAngelesTime = moment().tz("America/Los_Angeles");
+
+        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+        losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+
+    }
+
+    let parisElement = document.querySelector("#paris");
+    if (parisElement) {
+        let parisDateElement = parisElement.querySelector(".date");
+        let parisTimeElement = parisElement.querySelector(".time");
+        let parisTime = moment().tz("Europe/Paris");
+
+        parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+        parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+    }
+
+    let mexicoElement = document.querySelector("#mexico");
+    if (mexicoElement) {
+        let mexicoDateElement = mexicoElement.querySelector(".date");
+        let mexicoTimeElement = mexicoElement.querySelector(".time");
+        let mexicoTime = moment().tz("America/Mexico_City");
+
+        mexicoDateElement.innerHTML = mexicoTime.format("MMMM Do YYYY");
+        mexicoTimeElement.innerHTML = mexicoTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 }
 
-setTimeout{ sayHello, 3000 };
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+    }
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+        <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+        "A"
+    )}</small></div>
+  </div>
+  <a href="/">All cities</a>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
